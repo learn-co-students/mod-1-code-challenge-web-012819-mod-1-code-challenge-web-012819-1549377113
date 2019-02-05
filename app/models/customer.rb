733a7560e4a5730811a9211c7a133c4110926e01
github username <github email address>
2019-02-05 10:37:13 -0500

    finished

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,55 @@
 class Customer
   attr_accessor :first_name, :last_name
-
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(full_name)
+    self.all.find do |customer|
+    (customer.first_name + ' ' + customer.last_name)  == full_name
+    end
+  end
+
+  def self.find_all_by_first_name(first_name)
+    self.all.select do |customer|
+      customer.first_name == first_name
+    end
+  end
+
+def self.all_names
+self.all.map do |customer|
+(customer.first_name + ' ' + customer.last_name)
+  end
+end
+
+def add_review(restaurant, content, rating)
+Review.new(self, restaurant, rating, content)
+end
+
+def num_reviews
+my_reviews = Review.all.select do |review|
+review.customer == self
+end
+my_reviews.length
+end
+
+def restaurants
+Review.all.map do |review|
+  if review.customer == self
+    review.restaurant
+  end
+end.compact.uniq
+end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,59 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |rest|
+      rest.name == name
+  end
+end
+
+def customers
+array = Review.all.map do |review|
+  if review.restaurant== self
+    review.customer
+end
+end.compact.uniq
+end
+
+def reviews
+Review.all.select do |review|
+review.restaurant == self
+end
+end
+
+def average_star_rating
+  sum = 0
+
+  my_ratings = Review.all.map do |review|
+  if review.restaurant == self
+    review.rating
+end
+end.compact
+my_ratings.each do |num|
+  sum += num
+end
+(sum/my_ratings.length).to_f
+
+end
+
+def longest_review
+  longest_content = 0
+  Review.all.each do |review|
+    if review.restaurant == self && review.content.length > longest_content
+      longest_content = review.content.length
+    end
+  end
+  longest_content
+end
 
 end
